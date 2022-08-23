2ba68fdbf4c7a9f136f422c35abb985d424c2b1a
Mahinich <andrey.mahinitch@gmail.com>
2022-08-23 15:18:15 +0300

    section our trip is done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,3 @@
 $((function(){$(".fairy-tail__slider").slick({prevArrow:'<button type="button" class="slick-btn slick-prev"><img src="icons/arrow-left.svg" alt="prev"></button>',nextArrow:'<button type="button" class="slick-btn slick-next"><img src="icons/arrow-right.svg" alt="next"></button>',autoplay:!0,autoplaySpeed:2e3,fade:!0})}));
+
+$((function(){$(".our-trip__slider").slick({prevArrow:'<button type="button" class="slick-btn slick-prev"><img src="icons/arrow-left.svg" alt="prev"></button>',nextArrow:'<button type="button" class="slick-btn slick-next"><img src="icons/arrow-right.svg" alt="next"></button>',autoplay:!0,autoplaySpeed:2e3,fade:!0})}));
